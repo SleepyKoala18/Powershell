6b04e279775de5ff3b0988c74a826b4a309b3633
SleepyKoala18 <163049851+SleepyKoala18@users.noreply.github.com>
2024-03-25 13:38:13 -0400

    Update Practice2.ps1

--- Practice2.ps1
+++ Practice2.ps1
@@ -1,1 +1,13 @@
+$var1 = Get-Random -Minimum 25 -Maximum 50
+$var1
+$var2 = Get-Random -Minimum 1 -Maximum 10
+$var2
+$sum = $var1 + $var2
+$sum
 
+$sum = $var1 * $var2
+$prod = $var1 * $var2
+$quot = $var / $var2 
+
+
+write output" $var1 + $var2 = $sum"
