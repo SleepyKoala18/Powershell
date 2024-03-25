f3a352e38cb2c610eb68614c64295cd0f512ced2
SleepyKoala18 <163049851+SleepyKoala18@users.noreply.github.com>
2024-03-25 10:18:13 -0400

    Create Practice.ps1

---
+++ Practice.ps1
@@ -0,0 +1,40 @@
+    Ensure that you have the latest copy of help by updating your help system.
+    Update-help
+    Which cmdlets deal with the viewing/manipulating of processes?
+    Get-process start,stop,wait
+    Display a list of services installed on your local computer.
+    get-service
+    What cmdlets are used to write or output objects or text to the screen?
+    write-output
+    What cmdlets can be used to create, modify, list, and delete variables?
+    new-variable get-variable remove-variable 
+    What cmdlet can be used, other than Get-Help, to find and list other cmdlets?
+    get-alias 
+    Find the cmdlet that is used to prompt the user for input.
+    read-host 
+
+
+
+
+    Display a list of running processes.
+    get-process
+    Display a list of all running processes that start with the letter "s".
+    get-process -name s*
+    Find the cmdlet and its purpose for the following aliases:
+
+        gal
+
+        dir
+
+        echo
+
+        ?
+
+        %
+
+        ft
+
+    Display a list of Windows Firewall Rules.
+    get-netfirewallrule
+    Create a new alias called "gh" for the cmdlet "Get-Help"
+    set-alaias gh get-help
