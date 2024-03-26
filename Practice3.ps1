1597eabc34599d58d94102bf4ac21f12800e08dc
SleepyKoala18 <163049851+SleepyKoala18@users.noreply.github.com>
2024-03-26 12:57:35 -0400

    Update Practice3.ps1

--- Practice3.ps1
+++ Practice3.ps1
@@ -4,3 +4,11 @@ $array = $array1..$array2
 
 $arrayrev = $array2..$array1 
 $arrayrev
+
+OR 
+
+Reverse Arrays 
+$array = (Get-Random - minimum -10 -Maximum 1)..`
+(Get-Random -minimum 1 -maximum 21)
+
+$reverse = $array[($array.lenght -1)..0] 
