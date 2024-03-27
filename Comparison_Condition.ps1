2a6a858c5a2b22781a8ce38b3458d5d688a88871
SleepyKoala18 <163049851+SleepyKoala18@users.noreply.github.com>
2024-03-27 09:16:12 -0400

    Create Comparison_Condition.ps1

---
+++ Comparison_Condition.ps1
@@ -0,0 +1,13 @@
+$line1 = "Do you have model number: MT5437 for john.doe@sharklasers.com?"
+$line2 = "What model number for john.doe@sharklasers.com?"
+
+
+$pattern = '[A-Za-z]{2}\d{2,5}'
+$line1, $line2 | ForEach-obect {
+if ($_ -match $pattern){
+    Write-Host $Matches[0]": $_"
+}
+else{
+    write-host "no matches found on: $_"
+ }
+}  
